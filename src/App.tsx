ad5e489e29c602afd7eca5de9d8c4bd264cf8b39
fuateraslann <fuatze@gmail.com>
2023-11-26 15:21:17 +0300

    theme and font added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import './App.css';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import Header from 'Layout/Header'
+import './App.css'
 function App() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'Roboto, sans-serif',
+      button: {
+        textTransform: 'none',
+      },
+    },
+    palette: {
+      primary: {
+        light: '#f44336',
+        main: '#c1135cd6',
+        dark: '#c1135ba',
+        contrastText: '#fff',
+      },
+    },
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header />
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
           Learn React
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
