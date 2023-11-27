fb69c44ca4ce19e2331fa9d286381279601ae46b
fuateraslann <fuatze@gmail.com>
2023-11-27 17:10:32 +0300

    cart action added to modal

--- src/components/Cart/CartProduct.tsx
+++ src/components/Cart/CartProduct.tsx
@@ -9,12 +9,14 @@ import {
 } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import RemoveIcon from '@mui/icons-material/Remove'
+import { useQueryClient } from 'react-query'
 import { TCartProduct } from 'types'
+import { useAddToCart, useSubtractFromCart } from 'hooks/endpoints'
 
 interface IQuantityButtonsProps {
   quantity: number
-  onIncrease?: () => void
-  onDecrease?: () => void
+  onIncrease: () => void
+  onDecrease: () => void
 }
 const QuantityButtons = ({
   quantity,
@@ -23,7 +25,7 @@ const QuantityButtons = ({
 }: IQuantityButtonsProps) => {
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
-      <IconButton onClick={onDecrease} disabled={quantity === 1}>
+      <IconButton onClick={onDecrease}>
         <RemoveIcon />
       </IconButton>
       <Typography variant="body1" style={{ margin: '0 8px' }}>
@@ -38,6 +40,20 @@ const QuantityButtons = ({
 
 const CartProduct = ({ product }: { product: TCartProduct }) => {
   const theme = useTheme()
+  const queryClient = useQueryClient()
+  const { mutateAsync: addToCart } = useAddToCart()
+  const { mutateAsync: subtractFromCart } = useSubtractFromCart()
+  const handleIncrease = async () => {
+    await addToCart({ id: product.productId })
+    queryClient.refetchQueries('viewCart')
+  }
+
+  const handleDecrease = async () => {
+    if (product.quantity > 0) {
+      await subtractFromCart({ id: product.productId })
+      queryClient.refetchQueries('viewCart')
+    }
+  }
   return (
     <ListItem>
       <ListItemAvatar>
@@ -56,7 +72,11 @@ const CartProduct = ({ product }: { product: TCartProduct }) => {
         }
         sx={{ marginRight: 10 }}
       />
-      <QuantityButtons quantity={product.quantity} />
+      <QuantityButtons
+        quantity={product.quantity}
+        onDecrease={handleDecrease}
+        onIncrease={handleIncrease}
+      />
     </ListItem>
   )
 }
